a6fd9cc473b84a6d9c2d563068dde4ea82d3b82d
brina-malaquias <sabrinamalaquias@unitins.br>
2024-06-07 02:10:41 -0300

    ultimo ajuste

---
+++ src/main/java/br/unitins/topicos2/ano2024/form/ImageForm.java
@@ -0,0 +1,40 @@
+package br.unitins.topicos2.ano2024.form;
+
+
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.core.MediaType;
+
+public class ImageForm {
+    @FormParam("id")
+    private Long id;
+
+    @FormParam("nomeImagem")
+    private String nomeImagem;
+
+    @FormParam("imagem")
+    private byte[] imagem;
+
+    public String getNomeImagem() {
+        return nomeImagem;
+    }
+
+    public void setNomeImagem(String nomeImagem) {
+        this.nomeImagem = nomeImagem;
+    }
+
+    public byte[] getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(byte[] imagem) {
+        this.imagem = imagem;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }   
+}
